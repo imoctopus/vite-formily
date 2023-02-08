8d0827ed73f9c7cc8bca60c3ad67ff6f2c094900
imoctopus <ryker.zy@gmail.com>
2023-02-08 16:37:11 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,187 @@
+import React from "react";
+import {
+  FormItem,
+  Input,
+  Editable,
+  Select,
+  DatePicker,
+  ArrayItems,
+  FormButtonGroup,
+  Submit,
+  Space,
+} from "@formily/antd";
+import { createForm } from "@formily/core";
+import { FormProvider, createSchemaField } from "@formily/react";
+
+const SchemaField = createSchemaField({
+  components: {
+    FormItem,
+    DatePicker,
+    Editable,
+    Space,
+    Input,
+    Select,
+    ArrayItems,
+  },
+});
+
+const form = createForm();
+
+export default () => {
+  return (
+    <FormProvider form={form}>
+      <SchemaField>
+        <SchemaField.Array
+          name="string_array"
+          title="字符串数组"
+          x-decorator="FormItem"
+          x-component="ArrayItems"
+        >
+          <SchemaField.Void x-component="Space">
+            <SchemaField.Void
+              x-decorator="FormItem"
+              x-component="ArrayItems.SortHandle"
+            />
+            <SchemaField.String
+              x-decorator="FormItem"
+              required
+              name="input"
+              x-component="Input"
+            />
+            <SchemaField.Void
+              x-decorator="FormItem"
+              x-component="ArrayItems.Remove"
+            />
+            <SchemaField.Void
+              x-decorator="FormItem"
+              x-component="ArrayItems.Copy"
+            />
+          </SchemaField.Void>
+          <SchemaField.Void
+            x-component="ArrayItems.Addition"
+            title="添加条目"
+          />
+        </SchemaField.Array>
+        <SchemaField.Array
+          name="array"
+          title="对象数组"
+          x-decorator="FormItem"
+          x-component="ArrayItems"
+        >
+          <SchemaField.Object>
+            <SchemaField.Void x-component="Space">
+              <SchemaField.Void
+                x-decorator="FormItem"
+                x-component="ArrayItems.SortHandle"
+              />
+              <SchemaField.String
+                x-decorator="FormItem"
+                required
+                title="日期"
+                name="date"
+                x-component="DatePicker.RangePicker"
+                x-component-props={{
+                  style: {
+                    width: 160,
+                  },
+                }}
+              />
+              <SchemaField.String
+                x-decorator="FormItem"
+                required
+                title="输入框"
+                name="input"
+                x-component="Input"
+              />
+              <SchemaField.String
+                x-decorator="FormItem"
+                required
+                title="选择框"
+                name="select"
+                enum={[
+                  { label: "选项1", value: 1 },
+                  { label: "选项2", value: 2 },
+                ]}
+                x-component="Select"
+                x-component-props={{
+                  style: {
+                    width: 160,
+                  },
+                }}
+              />
+              <SchemaField.Void
+                x-decorator="FormItem"
+                x-component="ArrayItems.Remove"
+              />
+              <SchemaField.Void
+                x-decorator="FormItem"
+                x-component="ArrayItems.Copy"
+              />
+            </SchemaField.Void>
+          </SchemaField.Object>
+          <SchemaField.Void
+            x-component="ArrayItems.Addition"
+            title="添加条目"
+          />
+        </SchemaField.Array>
+        <SchemaField.Array
+          name="array2"
+          title="对象数组"
+          x-decorator="FormItem"
+          x-component="ArrayItems"
+          x-component-props={{ style: { width: 300 } }}
+        >
+          <SchemaField.Object x-decorator="ArrayItems.Item">
+            <SchemaField.Void
+              x-decorator="FormItem"
+              x-component="ArrayItems.SortHandle"
+            />
+            <SchemaField.String
+              x-decorator="Editable"
+              title="输入框"
+              name="input"
+              x-component="Input"
+              x-component-props={{ bordered: false }}
+            />
+            <SchemaField.Object
+              name="config"
+              x-component="Editable.Popover"
+              required
+              title="配置复杂数据"
+              x-reactions={(field: any) => {
+                field.title = field.value?.input || field.title;
+              }}
+            >
+              <SchemaField.String
+                x-decorator="FormItem"
+                required
+                title="日期"
+                name="date"
+                x-component="DatePicker.RangePicker"
+                x-component-props={{ style: { width: "100%" } }}
+              />
+              <SchemaField.String
+                x-decorator="FormItem"
+                required
+                title="输入框"
+                name="input"
+                x-component="Input"
+              />
+            </SchemaField.Object>
+            <SchemaField.Void
+              x-decorator="FormItem"
+              x-component="ArrayItems.Remove"
+            />
+          </SchemaField.Object>
+          <SchemaField.Void
+            x-component="ArrayItems.Addition"
+            title="添加条目"
+          />
+        </SchemaField.Array>
+      </SchemaField>
+      <FormButtonGroup>
+        <Submit onSubmit={console.log}>提交</Submit>
+      </FormButtonGroup>
+    </FormProvider>
+  );
+};
